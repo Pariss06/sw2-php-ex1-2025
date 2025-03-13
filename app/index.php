5b5498d763950f8ea4b48bb922c51e6f97dac9b5
Pariss06 <willianparis834@gmail.com>
2025-03-13 01:43:54 +0000

    Aula 12/03/2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,67 @@
 <?php
-// require './vendor/autoload.php';
+//$nome = $_GET['nome'];
+    $nome = ' ';
+    $periodo = ' ';
+    $mensagem = ' ';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $periodo = $_POST['periodo'];
+    switch($periodo) {
+        case 'm':
+            $mensagem = "Bom Dia $nome";
+        break;
+        case 't':
+            $mensagem = "Boa Tarde $nome";
+        break;
+        case 'n':
+            $mensagem = "Boa Noite $nome";
+        break;
+    }
+}
+?>
 
-// use Application\DBConnection\MySQLConnection;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Primeira Aplicação</title>
+</head>
+    <body>
+        <h1>
+            <?php
+            if($periodo == 'm'){
+                echo ' Bom Dia ' . $nome;
+            }elseif ($periodo == 't'){
+                echo ' Boa Tarde ' . $nome;
+            }elseif ($periodo == 'n'){
+                echo ' Boa Noite ' . $nome;
+            }
+            ?>
+        </h1>
 
-// $db = new MySQLConnection();
+        <h1>
+            <?php if($periodo =='m'): ?>
+                Bom dia <?= $nome ?>
+            <?php elseif($periodo == 't'): ?>
+                Boa tarde <?= $nome ?>
+            <?php elseif($periodo == 'n'): ?>
+                Boa noite <?= $nome ?>
+            <?php endif ?>
+        </h1>
+        <h1><?= $mensagem ?> </h1>
 
-echo phpinfo();
+        <form action="/index.php" method="POST">
+             <label for="periodo"> Selecione o Período </label>
+                <select name="periodo">
+                    <option value="m"> Manhã </option>
+                    <option value="t"> Tarde </option>
+                    <option value="n"> Noite </option>
+                </select>
+        <br>
+
+            <label for="nome">Digite seu nome</label>
+                <input type="text" name="nome" />
+            <button type="submit"> Enviar </button>
+        </form>
+    </body>
+</html>
